a46496af9a38f0b25e47644dce72a9e53f57fc45
MarcelGehrig <marcel.gehrig@ntb.ch>
2017-08-14 14:26:07 +0200

    nanosleep test

--- main.cpp
+++ main.cpp
@@ -17,7 +17,8 @@
 
 #include "control/controlSystem/MyControlSystem.hpp"
 #include "control/safetySystem/MySafetyProperties.hpp"
-
+#include <chrono>
+#include <time.h>
 using namespace eeros;
 using namespace eeros::logger;
 
@@ -50,6 +51,30 @@ int main(int argc, char **argv) {
 	ros::init(dummy_argc, dummy_args, "EEROSNode");
 	log.trace() << "ROS node initialized.";
 	ros::NodeHandle rosNodeHandler;
+
+
+// 	int milisec = 100; // length of time to sleep, in miliseconds
+struct timespec req = {0};
+req.tv_sec = 0;
+req.tv_nsec = 100000;
+
+typedef std::chrono::steady_clock clock;
+auto start = clock::now();
+// usleep(50);
+nanosleep(&req, (struct timespec *)NULL);
+nanosleep(&req, (struct timespec *)NULL);
+nanosleep(&req, (struct timespec *)NULL);
+nanosleep(&req, (struct timespec *)NULL);
+nanosleep(&req, (struct timespec *)NULL);
+nanosleep(&req, (struct timespec *)NULL);
+nanosleep(&req, (struct timespec *)NULL);
+nanosleep(&req, (struct timespec *)NULL);
+nanosleep(&req, (struct timespec *)NULL);
+nanosleep(&req, (struct timespec *)NULL);
+auto stop = clock::now();
+std::chrono::duration<int, std::nano> duration = stop - start;	//nsec allways int.
+//std::chrono::duration<double, std::milli> duration = stop - start;	//cast to msec
+std::cout << "Duration: " << duration.count() << " nsec" << std::endl;
 		
 	// Control System
 	// ////////////////////////////////////////////////////////////////////////
